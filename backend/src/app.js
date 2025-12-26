248ee5475e2c20e32f024d571f070b5df2f909d8
GAURAV SHUKLA <shuklag868@gmail.com>
2025-12-27 02:55:14 +0530

    last commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const inventoryRoutes = require('./routes/inventory.routes');
-const salesRoutes = require('./routes/sales.routes');
-const analyticsRoutes = require('./routes/analytics.routes');
-const auditRoutes = require('./routes/audit.routes');
-const errorHandler = require('./middlewares/error.middleware');
+const express = require("express");
+const cors = require("cors");
+const inventoryRoutes = require("./routes/inventory.routes");
+const salesRoutes = require("./routes/sales.routes");
+const analyticsRoutes = require("./routes/analytics.routes");
+const auditRoutes = require("./routes/audit.routes");
+const errorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 
-app.use(cors());
+// ALLOW ORIGINS FROM FRONTEND URL
+const allowedOrigins = [process.env.FRONTEND_URL];
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
 app.use(express.json());
 
 // Routes
-app.use('/api/inventory', inventoryRoutes);
-app.use('/api/sales', salesRoutes);
-app.use('/api/analytics', analyticsRoutes);
-app.use('/api/audit', auditRoutes);
+app.use("/api/inventory", inventoryRoutes);
+app.use("/api/sales", salesRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/audit", auditRoutes);
 
 // Error Handling
 app.use(errorHandler);
